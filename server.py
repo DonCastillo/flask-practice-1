f3383ac04fb628e2ed98fc548149a50784a3e5c5
DonCastillo <don.qcastillo@yahoo.com>
2026-07-04 21:39:28 -0400

    learned setting custon status code

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask("Hello, World")
 
 @app.route('/')
 def index():
     return {"message": "Hello, World!"}
+
+@app.route('/no_content')
+def no_content():
+    return (dict(message="No content found"), 204)
+
+@app.route('/exp')
+def index_explicit():
+    resp = make_response(dict(message="Hello World"))
+    resp.status_code = 200
+    return resp
